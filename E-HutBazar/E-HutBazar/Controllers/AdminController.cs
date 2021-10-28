f06b9bf7243d5ec144829e1be86e8d7901218a65
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2021-10-29 03:26:10 +0600

    Login system is done for admin

--- E-HutBazar/E-HutBazar/Controllers/AdminController.cs
+++ E-HutBazar/E-HutBazar/Controllers/AdminController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using E_HutBazar.Models;
+using E_HutBazar.Repository;
 
 namespace E_HutBazar.Controllers
 {
@@ -10,6 +13,23 @@ namespace E_HutBazar.Controllers
     {
         // GET: Admin
         public ActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User_Admin a)
+        {
+            var user = AdminUserRepository.Authenticate(a.Admin_Username,a.Admin_Password);
+            if (user != null)
+            {
+                var data = AdminUserRepository.Authenticate(a.Admin_Username, a.Admin_Password);
+                FormsAuthentication.SetAuthCookie(data.Admin_Username, true);
+                return RedirectToAction("Dashboard");
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Dashboard()
         {
             return View();
         }
---
+++ E-HutBazar/E-HutBazar/Repository/AdminUserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using E_HutBazar.Models;
+
+namespace E_HutBazar.Repository
+{
+    public class AdminUserRepository
+    {
+        static EhutBazardbEntities db;
+        static AdminUserRepository()
+        {
+            db = new EhutBazardbEntities();
+        }
+        public static User_Admin Authenticate(string userName, string password)
+        {
+            var c = (from ad in db.User_Admin
+                     where ad.Admin_Username == userName &&
+                     ad.Admin_Password == password
+                     select ad).FirstOrDefault();
+
+            
+            return c;
+        }
+    }
+}
