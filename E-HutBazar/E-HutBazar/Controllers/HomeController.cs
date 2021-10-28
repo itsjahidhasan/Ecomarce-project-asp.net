46b06a526f845a0790605f1e6fde9e9d9f5b66c9
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2021-10-28 17:05:25 +0600

    Bootstrab V4.6.0

--- E-HutBazar/E-HutBazar/Controllers/HomeController.cs
+++ E-HutBazar/E-HutBazar/Controllers/HomeController.cs
@@ -8,11 +8,11 @@ namespace E_HutBazar.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
